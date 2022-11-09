8aeb9c6c61bc6b431035884986e6840fda2ecf38
Stu <stumac1000@yahoo.com>
2022-11-09 13:59:16 -0800

    Setting up visual layout

---
+++ script.js
@@ -0,0 +1,5 @@
+// Limit the number of letters someone can input for their name so it doesn't
+// break the grid layout, make it 8 letters with all letters lowercase accept
+// the first letter is capitalized.
+
+//Color the X's and O's red and blue respectively
