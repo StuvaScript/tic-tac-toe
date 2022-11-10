aba1a422d3c450a157606cd1fb0744572414907a
Stu <stumac1000@yahoo.com>
2022-11-10 11:07:54 -0800

    Create new branch

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Split the whole project to another brach when you begin fucking with 
+// JavaScript
+
+
 // Limit the number of letters someone can input for their name so it doesn't
 // break the grid layout, make it 8 letters with all letters lowercase accept
 // the first letter is capitalized.
