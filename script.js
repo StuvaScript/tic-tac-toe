6067a1831667efe0f14542079ac466c265b2fa67
Stu <stumac1000@yahoo.com>
2022-11-11 13:38:38 -0800

    Add more functionality

--- script.js
+++ script.js
@@ -29,22 +29,57 @@ const displayController = (() => {
     _getElement.forEach(element => {
         element.addEventListener('click', (e) => {
 
-            // X's or O's
-            const switchSymbol = () => {                
-                (_playerTurn === undefined || _playerTurn === 'X') ? _playerTurn = 'O' : _playerTurn = 'X';
-                return _playerTurn;
-            };
+            if (e.target.innerText === '') {
+                // X's or O's
+                const switchSymbol = () => {     
+                    (_playerTurn === undefined || _playerTurn === 'X') ? _playerTurn = 'O' : _playerTurn = 'X';
+                    return _playerTurn;
+                };
 
-            // Adds X's or O's to gameBoard.board aray
-            gameBoard.board.splice(`${e.target.getAttribute('data-array')}`, 1, switchSymbol())
-            
-            // Cycles thru the array and displays X's or O's on the HTML
-            gameBoard.board.forEach((value, index) => {
-                const _getIndex = _getElement[`${index}`];
-                _getIndex.innerText = `${value}`;
-            })
+                // Adds X's or O's to gameBoard.board aray
+                gameBoard.board.splice(`${e.target.getAttribute('data-array')}`, 1, switchSymbol())
+                
+                // Cycles thru the array and displays X's or O's on the HTML
+                gameBoard.board.forEach((value, index) => {
+                    const _getIndex = _getElement[`${index}`];
+                    _getIndex.innerText = `${value}`;
+                    console.log(index + ' index');
+                    console.log(value + ' value');
+                })
+
+                // Working on the logic to a tie game....
+                
+                // Winning logic and announces winner.
+                const _winner = (() => {
+                    let whoWon;
+                    if (gameBoard.board[0] === 'X' && gameBoard.board[3] === 'X' && gameBoard.board[6] === 'X' ||
+                        gameBoard.board[1] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[7] === 'X' ||
+                        gameBoard.board[2] === 'X' && gameBoard.board[5] === 'X' && gameBoard.board[8] === 'X' ||
+                        gameBoard.board[0] === 'X' && gameBoard.board[1] === 'X' && gameBoard.board[2] === 'X' ||
+                        gameBoard.board[3] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[5] === 'X' ||
+                        gameBoard.board[6] === 'X' && gameBoard.board[7] === 'X' && gameBoard.board[8] === 'X' ||
+                        gameBoard.board[0] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[8] === 'X' ||
+                        gameBoard.board[2] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[6] === 'X') {
+                        whoWon = 'loser';
+                        console.log('Loser!!');
+                    } else if (gameBoard.board[0] === 'O' && gameBoard.board[3] === 'O' && gameBoard.board[6] === 'O' ||
+                        gameBoard.board[1] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[7] === 'O' ||
+                        gameBoard.board[2] === 'O' && gameBoard.board[5] === 'O' && gameBoard.board[8] === 'O' ||
+                        gameBoard.board[0] === 'O' && gameBoard.board[1] === 'O' && gameBoard.board[2] === 'O' ||
+                        gameBoard.board[3] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[5] === 'O' ||
+                        gameBoard.board[6] === 'O' && gameBoard.board[7] === 'O' && gameBoard.board[8] === 'O' ||
+                        gameBoard.board[0] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[8] === 'O' ||
+                        gameBoard.board[2] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[6] === 'O') {
+                        whoWon = 'winner';
+                        console.log('Winner!!');
+                    } 
+                    
+                console.log(gameBoard.board);
+                })();
+                
+            };    
         });
     })
-    // Delete this return if you dont need to actually return anything
+    // Delete this return if you don't need to actually return anything
     return {};
 })();
